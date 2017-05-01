e0cc074edc73f10c9fee1627c1a46a948234ad24
Gagandeep Singh <gagandeepsinghsaini65@gmail.com>
2017-05-01 15:52:43 +1200

    Signup and LOgin Page

---
+++ One Real Estate - Database.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace One_Real_Estate___Database.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ One Real Estate - Database/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="One_Real_Estate___Database.Global" Language="C#" %>
